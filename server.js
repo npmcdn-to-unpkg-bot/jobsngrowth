7af30a9152d45084f13038e76c164e5f025f440a
govhack-diis <peterst6906@gmail.com>
2016-07-31 16:18:42 +1000

    port adjust

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('GovHack is awesome on Port 3000');
 });
